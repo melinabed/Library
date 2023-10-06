e108a432500af4c922253db0b130f0022ea4f492
melinabed <melo36049@hotmail.com>
2023-10-06 13:30:06 -1000

    made toggleRead render

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 // the contructor
 function Book(title, author, pages, read) {
@@ -8,16 +8,44 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+//grabs contructor and applies to toggleRead function
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead();
+    render();
+}
+
 // grabs the cards div and applies new element
+// uses dom to create a class for book-card
 function render() {
     let libraryEl = document.querySelector("#cards");
     libraryEl.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
         let book = myLibrary[i];
         let bookEl = document.createElement("div");
-        bookEl.innerHTML = `<p>${book.title}</p>`
+        bookEl.setAttribute("class", "book-card");
+        bookEl.innerHTML = `
+            <div class="card-header">
+            <h3 class="title">${book.title}</h3>
+            <h5 class="author">by ${book.author}</h5>
+            </div>
+            <div class="card-body">
+            <p>${book.pages} pages</p>
+            <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+            <button class="remove" onclick="removeBook(${i})">Remove</button>
+            <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+            </div>
+            `;
         libraryEl.appendChild(bookEl);
     }
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    render();
 }
 
 
@@ -36,7 +64,7 @@ function addBookToLibrary() {
 let newBook = document.querySelector("#new");
 newBook.addEventListener('click', function() {
     let form = document.querySelector("#book-info");
-    form.style.display = "flex";
+    form.style.display = "block";
 })
 
 // applies click listener to submit button and calls the addBooktoLibrary function
